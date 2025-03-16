d5f25a3025e530444edb3ce80914a2f4858ae2e1
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-03-16 18:20:34 +0800

    feat: Log correct server URL based on environment

--- app.js
+++ app.js
@@ -10,7 +10,11 @@ app.listen(PORT, (error) => {
 	if (error) {
 		console.error(`Failed to start server: ${error}`);
 	} else {
-		console.log(`Server is listening on: http://localhost:${PORT}`);
+		const baseUrl =
+			process.argv[2] === "PRODUCTION"
+				? process.env.PRODUCTION_URL
+				: `http://localhost:${PORT}`;
+		console.log(`Server is listening on: ${baseUrl}`);
 	}
 });
 
