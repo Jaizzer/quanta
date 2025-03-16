560908d59331229c5cc9fcc35be87618348d3870
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-03-16 11:35:51 +0800

    ref: Rearrange code for readability

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-const express = require("express");
-const dotenv = require("dotenv");
-
 // Load environment variables
+const dotenv = require("dotenv");
 dotenv.config();
 
 // Setup the server
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, (error) => {
