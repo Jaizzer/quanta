e42f399c65496e672b250da9ae47caed28d63bb9
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-03-27 11:49:29 +0800

    feat: Include the newly created route for individual transactions

--- routes/transactionsRouter.js
+++ routes/transactionsRouter.js
@@ -3,5 +3,6 @@ const transactionsRouter = Router();
 const transactionsControllers = require("../controllers/transactionsControllers.js");
 
 transactionsRouter.get("/", transactionsControllers.getAllTransactions);
+transactionsRouter.get("/:id(\\d+)", transactionsControllers.getTransactionById)
 
 module.exports = transactionsRouter;
