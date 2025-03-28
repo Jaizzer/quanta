475186329804a39ac57d2179c5af7864f6b50ae6
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-03-28 08:44:56 +0800

    feat: Add the items route

--- routes/itemsRouter.js
+++ routes/itemsRouter.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const itemsRouter = Router();
 const itemsControllers = require("../controllers/itemsControllers.js");
 
+itemsRouter.get("/", itemsControllers.getAllItems);
 itemsRouter.get("/:id(\\d+)", itemsControllers.getItemById);
 
 module.exports = itemsRouter;
