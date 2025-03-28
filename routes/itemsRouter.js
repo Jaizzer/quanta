76a3482ebe3906abc58af76000c7a04bed301ef4
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-03-28 17:19:02 +0800

    feat: Create route for adding item

--- routes/itemsRouter.js
+++ routes/itemsRouter.js
@@ -3,6 +3,7 @@ const itemsRouter = Router();
 const itemsControllers = require("../controllers/itemsControllers.js");
 
 itemsRouter.get("/", itemsControllers.getAllItems);
+itemsRouter.get("/add-item", itemsControllers.addItemGet);
 itemsRouter.get("/:id", itemsControllers.getItemById);
 
 module.exports = itemsRouter;
