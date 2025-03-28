ea5561fac8608a790af55b7de8b57f155036633b
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-03-28 08:44:42 +0800

    feat: Create controller to render all items in items page

--- controllers/itemsControllers.js
+++ controllers/itemsControllers.js
@@ -1,4 +1,40 @@
 const asyncHandler = require("express-async-handler");
+const getTotalItemQuantity = require("./getTotalItemQuantity");
+const getTotalValue = require("./getTotalValue");
+
+async function getAllItems(req, res, next) {
+	const items = [
+		{
+			id: 24,
+			name: "Item 78",
+			quantity: 5,
+			minLevel: 1,
+			tags: ["Tag 1", "Tag2"],
+			updatedAt: "Mar 20",
+			price: 25,
+			totalValue: 50,
+		},
+
+		{
+			id: 25,
+			name: "Item 99",
+			quantity: 5,
+			minLevel: 1,
+			tags: ["Tag 1", "Tag3"],
+			updatedAt: "Mar 20",
+			price: 25,
+			totalValue: 50,
+		},
+	];
+
+	res.render("items", {
+		title: "Items",
+		items: items,
+		totalQuantity: getTotalItemQuantity(items),
+		totalValue: getTotalValue(items),
+		distinctItemQuantity: items.length,
+	});
+}
 
 async function getItemById(req, res, next) {
 	const itemId = Number(req.params.id);
@@ -25,4 +61,5 @@ async function getItemById(req, res, next) {
 
 module.exports = {
 	getItemById: asyncHandler(getItemById),
+	getAllItems: asyncHandler(getAllItems),
 };
