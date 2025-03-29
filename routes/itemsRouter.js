09395c09dca8cb4c9180dbc6d4b6162e0ebab4c8
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-03-30 06:12:36 +0800

    feat: Add the route for add-item post request

--- routes/itemsRouter.js
+++ routes/itemsRouter.js
@@ -4,6 +4,7 @@ const itemsControllers = require("../controllers/itemsControllers.js");
 
 itemsRouter.get("/", itemsControllers.getAllItems);
 itemsRouter.get("/add-item", itemsControllers.addItemGet);
+itemsRouter.post("/add-item", itemsControllers.addItemPost);
 itemsRouter.get("/:id", itemsControllers.getItemById);
 
 module.exports = itemsRouter;
