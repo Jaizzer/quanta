5b9fad282143027aede642fe23d8767b85204a81
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-04-03 13:27:45 +0800

    feat: Enable the item variant toggle button to hide or show the variant creation section

--- public/enableItemVariantCreation.js
+++ public/enableItemVariantCreation.js
@@ -1,12 +1,33 @@
-let isVariantCreationEnabled = false;
+let isVariantCreationEnabled = true;
 
 let variantAddingToggleBtn = document.querySelector(
 	".variant-adding-toggle-btn",
 );
+
+// Access the form
+const form = document.querySelector("form");
+
+// Clone of the current variant creation section
+let currentVariantCreationSectionCopy;
 
 variantAddingToggleBtn.addEventListener("click", () => {
 	isVariantCreationEnabled = !isVariantCreationEnabled;
 	variantAddingToggleBtn.textContent = isVariantCreationEnabled
 		? "Disable Item Variant"
 		: "Enable Item Variant";
+
+	if (!isVariantCreationEnabled) {
+		// Backup the variant creation section first before removal
+		let currentVariantCreationSection = document.querySelector(
+			".variant-creation-container",
+		);
+		currentVariantCreationSectionCopy =
+			currentVariantCreationSection.cloneNode(true);
+
+		// Remove the original variant creation Section
+		form.removeChild(currentVariantCreationSection);
+	} else {
+		// Restore the variant creation section from a created copy
+		form.appendChild(currentVariantCreationSectionCopy);
+	}
 });
