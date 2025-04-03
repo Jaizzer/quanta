c1b8b68cb8305b7c35303db5511b58d50c2ab493
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-04-03 16:22:15 +0800

    feat: Move the logic of adding variant field into a function

--- public/enableItemVariantCreation.js
+++ public/enableItemVariantCreation.js
@@ -5,61 +5,9 @@ let variantAddingToggleBtn = document.querySelector(
 );
 
 let addVariantBtn = document.querySelector(".add-variant-btn");
+
 addVariantBtn.addEventListener("click", () => {
-	// Extract the last variant field
-	const lastVariantField = Array.from(
-		document.querySelectorAll(".variant-field"),
-	).slice(-1)[0];
-
-	// Extract the input field name
-	const inputFieldName = lastVariantField.querySelector("input").name;
-
-	// Extract the ID from the input field name
-	const id = parseInt(inputFieldName.slice(-1));
-
-	// Create the id of the new variant field to be created
-	const newVariantFieldId = `Variant-${id + 1}`;
-
-	// Clone the last variant field that will serve as the new variant field
-	const newVariantField = lastVariantField.cloneNode(true);
-
-	// Access all the inputs inside the new variant field input
-	const newVariantFieldInputs = Array.from(
-		newVariantField.querySelectorAll("input"),
-	);
-
-	// Update the name of all the inputs to match the newly generated id
-	newVariantFieldInputs.forEach((input) => (input.name = newVariantFieldId));
-
-	// Put an initial value inside the variant name input
-	newVariantFieldInputs[0].value = newVariantFieldId;
-
-	// Remove any error messages copied from the last variant field
-	let errorMessageCopiedFromLastVariantField =
-		newVariantField.querySelector(".error-message");
-	if (errorMessageCopiedFromLastVariantField) {
-		newVariantField.removeChild(errorMessageCopiedFromLastVariantField);
-	}
-
-	// Add event listener to the new variant field's Delete button
-	const newVariantFieldDeleteBtn = newVariantField.querySelector(
-		".delete-variant-btn",
-	);
-
-	newVariantFieldDeleteBtn.addEventListener("click", (e) => {
-		e.target.parentElement.parentElement.removeChild(
-			e.target.parentElement,
-		);
-	});
-
-	// Place the newly created variant field next to the last variant field
-	let variantCreationSection = document.querySelector(
-		".variant-creation-container",
-	);
-	variantCreationSection.insertBefore(
-		newVariantField,
-		variantCreationSection.querySelector(".add-variant-btn"),
-	);
+	addVariant();
 });
 
 let deleteVariantBtns = Array.from(
@@ -116,3 +64,60 @@ variantAddingToggleBtn.addEventListener("click", () => {
 		form.appendChild(currentVariantCreationSectionCopy);
 	}
 });
+
+function addVariant() {
+	// Extract the last variant field
+	const lastVariantField = Array.from(
+		document.querySelectorAll(".variant-field"),
+	).slice(-1)[0];
+
+	// Extract the input field name
+	const inputFieldName = lastVariantField.querySelector("input").name;
+
+	// Extract the ID from the input field name
+	const id = parseInt(inputFieldName.slice(-1));
+
+	// Create the id of the new variant field to be created
+	const newVariantFieldId = `Variant-${id + 1}`;
+
+	// Clone the last variant field that will serve as the new variant field
+	const newVariantField = lastVariantField.cloneNode(true);
+
+	// Access all the inputs inside the new variant field input
+	const newVariantFieldInputs = Array.from(
+		newVariantField.querySelectorAll("input"),
+	);
+
+	// Update the name of all the inputs to match the newly generated id
+	newVariantFieldInputs.forEach((input) => (input.name = newVariantFieldId));
+
+	// Put an initial value inside the variant name input
+	newVariantFieldInputs[0].value = newVariantFieldId;
+
+	// Remove any error messages copied from the last variant field
+	let errorMessageCopiedFromLastVariantField =
+		newVariantField.querySelector(".error-message");
+	if (errorMessageCopiedFromLastVariantField) {
+		newVariantField.removeChild(errorMessageCopiedFromLastVariantField);
+	}
+
+	// Add event listener to the new variant field's Delete button
+	const newVariantFieldDeleteBtn = newVariantField.querySelector(
+		".delete-variant-btn",
+	);
+
+	newVariantFieldDeleteBtn.addEventListener("click", (e) => {
+		e.target.parentElement.parentElement.removeChild(
+			e.target.parentElement,
+		);
+	});
+
+	// Place the newly created variant field next to the last variant field
+	let variantCreationSection = document.querySelector(
+		".variant-creation-container",
+	);
+	variantCreationSection.insertBefore(
+		newVariantField,
+		variantCreationSection.querySelector(".add-variant-btn"),
+	);
+}
