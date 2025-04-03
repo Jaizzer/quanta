f1c7044ea5462d234a9a66696b2cf316e891eeb1
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-04-03 13:57:56 +0800

    feat: Create an event listener that enables the Add Variant button to insert a new variant field

--- public/enableItemVariantCreation.js
+++ public/enableItemVariantCreation.js
@@ -4,6 +4,53 @@ let variantAddingToggleBtn = document.querySelector(
 	".variant-adding-toggle-btn",
 );
 
+let addVariantBtn = document.querySelector(".add-variant-btn");
+addVariantBtn.addEventListener("click", () => {
+	// Extract the last variant field
+	const lastVariantField = Array.from(
+		document.querySelectorAll(".variant-field"),
+	).slice(-1)[0];
+
+	// Extract the input field name
+	const inputFieldName = lastVariantField.querySelector("input").name;
+
+	// Extract the ID from the input field name
+	const id = parseInt(inputFieldName.slice(-1));
+
+	// Create the id of the new variant field to be created
+	const newVariantFieldId = `Variant-${id + 1}`;
+
+	// Clone the last variant field that will serve as the new variant field
+	const newVariantField = lastVariantField.cloneNode(true);
+
+	// Access all the inputs inside the new variant field input
+	const newVariantFieldInputs = Array.from(
+		newVariantField.querySelectorAll("input"),
+	);
+
+	// Update the name of all the inputs to match the newly generated id
+	newVariantFieldInputs.forEach((input) => (input.name = newVariantFieldId));
+
+	// Put an initial value inside the variant name input
+	newVariantFieldInputs[0].value = newVariantFieldId;
+
+	// Remove any error messages copied from the last variant field
+	let errorMessageCopiedFromLastVariantField =
+		newVariantField.querySelector(".error-message");
+	if (errorMessageCopiedFromLastVariantField) {
+		newVariantField.removeChild(errorMessageCopiedFromLastVariantField);
+	}
+
+	// Place the newly created variant field next to the last variant field
+	let variantCreationSection = document.querySelector(
+		".variant-creation-container",
+	);
+	variantCreationSection.insertBefore(
+		newVariantField,
+		variantCreationSection.querySelector(".add-variant-btn"),
+	);
+});
+
 // Access the form
 const form = document.querySelector("form");
 
