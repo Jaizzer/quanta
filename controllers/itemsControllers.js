19d313fb4c5bfeaa6346b5e5259c587a11005c5f
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-04-06 06:11:36 +0800

    feat: Use the database data to render item

--- controllers/itemsControllers.js
+++ controllers/itemsControllers.js
@@ -176,21 +176,8 @@ async function getAllItems(req, res, next) {
 
 async function getItemById(req, res, next) {
 	const itemId = Number(req.params.id);
-	const items = [
-		{
-			id: 24,
-			name: "Item 78",
-			quantity: 5,
-			minLevel: 1,
-			tags: ["Tag 1", "Tag2"],
-			updatedAt: "Mar 20",
-			price: 25,
-			totalValue: 50,
-		},
-	];
-
-	const [item] = items.filter((item) => item.id === itemId);
-
+	const item = await db.getItemById(itemId);
+	item.updatedAt = "April 6, 2025";
 	res.render("item", {
 		title: item.name,
 		item: item,
--- models/queries.js
+++ models/queries.js
@@ -96,7 +96,80 @@ async function getAllItems() {
 	}
 }
 
+async function getItemById(itemID) {
+	try {
+		const query = `
+        SELECT 
+        id, 
+        name,
+        notify,
+        measurement,
+        price,
+        quantity,
+        notes, 
+        min_level,
+        variants,
+        total_value,
+        total_quantity,
+        JSONB_AGG(JSONB_BUILD_OBJECT('name', category_name, 'id', category_id) ORDER BY category_id) as categories
+        FROM (
+            SELECT 
+            items.id, 
+            items.name,
+            items.notify,
+            items.measurement,
+            items.price,
+            items.quantity,
+            items.notes, 
+            items.min_level,
+            categories.id as category_id,
+            categories.category as category_name,
+            SUM(variants.price * variants.quantity) as total_value,
+            SUM(variants.quantity) as total_quantity,
+            JSONB_AGG(JSONB_BUILD_OBJECT('name', variants.name, 'price', variants.price, 'quantity', variants.quantity) ORDER BY variants.name) AS variants
+            FROM items 
+            INNER JOIN item_categories 
+            ON item_categories.item_id = items.id 
+            INNER JOIN categories 
+            ON item_categories.category_id = categories.id 
+            INNER JOIN variants 
+            ON variants.parent_item_id = items.id
+            WHERE items.id = $1
+            GROUP BY 
+            items.id, 
+            items.name,
+            items.notify,
+            items.measurement,
+            items.price,
+            items.quantity,
+            items.notes,
+            items.min_level,
+            categories.category,
+            categories.id
+        )
+        GROUP BY 
+        id, 
+        name,
+        notify,
+        measurement,
+        price,
+        quantity,
+        notes, 
+        min_level,
+        variants,
+        total_value,
+        total_quantity
+        ;
+        `;
+		const { rows } = await pool.query(query, [itemID]);
+		return rows[0];
+	} catch (error) {
+		console.error("Error retrieving the item. ", error);
+	}
+}
+
 module.exports = {
 	insertItem,
 	getAllItems,
+	getItemById,
 };
