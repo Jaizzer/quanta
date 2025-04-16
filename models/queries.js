6dc74b02012a35ada49a016b7e4f47b164de27a7
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-04-17 07:13:29 +0800

    fix: Remove ".COUNT"

--- models/queries.js
+++ models/queries.js
@@ -146,7 +146,7 @@ async function getItemById(itemID) {
             SUM(variants.price * variants.quantity) as total_value,
             SUM(variants.quantity) as total_quantity,
             CASE
-                WHEN (SELECT COUNT(parent_item_id) FROM variants WHERE parent_item_id = $1).COUNT <> 0
+                WHEN (SELECT COUNT(parent_item_id) FROM variants WHERE parent_item_id = $1) <> 0
                     THEN JSONB_AGG(JSONB_BUILD_OBJECT('name', variants.name, 'price', variants.price, 'quantity', variants.quantity) ORDER BY variants.name)
                 ELSE NULL
             END AS variants
