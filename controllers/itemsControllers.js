b4f747d5a4e25b176b0f3df94272ad0419997c0f
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-04-26 06:02:04 +0800

    feat: Obtain the actual variant ID by removing the "Variant-" prefix

--- controllers/itemsControllers.js
+++ controllers/itemsControllers.js
@@ -252,7 +252,7 @@ async function editItemPost(req, res, next) {
 
 	// Extract all variants from the request body
 	let variants = variantInputNames.map((variantInputName) => ({
-		id: variantInputName,
+		id: parseInt(variantInputName.split("-")[1]),
 		name: req.body[variantInputName][0],
 		quantity: Math.abs(parseFloat(req.body[variantInputName][1])),
 		price: Math.abs(parseFloat(req.body[variantInputName][2])),
