86c738cb3597c1b9d9e643d9c71153aa4ce2bb01
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-05-08 10:08:29 +0800

    feat: Add js feature to close currently popped up warning message if a new delete button was pressed

--- public/deleteTag.js
+++ public/deleteTag.js
@@ -8,6 +8,12 @@ forms?.forEach((form) => {
 
 	// Render warning message before deleting the tag
 	deleteTagBtn.addEventListener("click", () => {
+		// Remove the current popup if there are any
+		const currentPopUp = document.querySelector(".background-overlay");
+		if (currentPopUp) {
+			currentPopUp.parentElement.removeChild(currentPopUp);
+		}
+
 		const backgroundOverlay = document.createElement("div");
 		backgroundOverlay.classList.add("background-overlay");
 		document.body.appendChild(backgroundOverlay);
